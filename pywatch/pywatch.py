1c234e34e4cba2f68da30628766765ee46e01f7f
Gonzalo Rafuls <grafuls@redhat.com>
2018-06-19 11:38:18 +0200

    First attempts at watcher
    Change-Id: I7bc3daa15611e88cf559e58b55ae4e7908e5668e

--- pywatch/pywatch.py
+++ pywatch/pywatch.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
-
 """Main module."""
+import subprocess
+import select
+
+
+class PyWatch(object):
+    # TODO: asynch ssh
+    def watch(self, filename):
+        f = subprocess.Popen(
+            ['tail', '-F', filename],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        p = select.poll()
+        p.register(f.stdout)
+
+        while True:
+            if p.poll(1):
+                yield f.stdout.readline()
